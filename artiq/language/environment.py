0f1ed18622d4fc771027bd2ffb6d069161c25767
Brendan Meins <brendan.meins@eleqtron.com>
2024-09-13 10:51:38 +0200

    generalize to DatasetToggleValue

--- artiq/language/environment.py
+++ artiq/language/environment.py
@@ -9,7 +9,7 @@ from artiq.language.core import rpc
 __all__ = ["NoDefault", "DefaultMissing",
            "PYONValue", "BooleanValue", "EnumerationValue",
            "NumberValue", "StringValue",
-           "HasEnvironment", "Experiment", "EnvExperiment", "PYONDatasetValue"]
+           "HasEnvironment", "Experiment", "EnvExperiment", "DatasetToggleValue"]
 
 
 class NoDefault:
@@ -200,22 +200,27 @@ class StringValue(_SimpleArgProcessor):
     pass
 
 
-class PYONDatasetValue(_SimpleArgProcessor):
-    """ Argument that has a list and boolean argument"""
+class DatasetToggleValue(_SimpleArgProcessor):
+    """ """
 
-    def __init__(self, argument: PYONValue = None,
+    def __init__(self, argument: _SimpleArgProcessor = None,
                  from_data_set: BooleanValue = None,
+                 data_set_value: any = None,
                  default=NoDefault):
+
         self.argument = argument
         self.from_data_set = from_data_set
+        self.data_set_value = data_set_value
         super().__init__(default)
 
     def process(self, x):
 
-        return {
-            "argument": self.argument.process(x["argument"]),
-            "from_data_set": self.from_data_set.process(x["from_data_set"])
-        }
+        from_data_set = self.from_data_set.process(x["from_data_set"])
+
+        if from_data_set:
+            return self.data_set_value
+        else:
+            return self.argument.process(x["argument"]),
 
     def describe(self):
         d = {"ty": self.__class__.__name__}
